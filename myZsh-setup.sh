cb981f986bf0ac06970695f0fcbaa71ad5c5f02e
Alex Koshelev <xrusts@gmail.com>
2017-11-29 16:23:56 +0500

    modify setup file

--- myZsh-setup.sh
+++ myZsh-setup.sh
@@ -1,13 +1,16 @@
-#!/bin/bash
+#! /bin/bash
 # check and install zsh support CentOS, Fedora an debian like systems.
- sudo which zsh || yum install zsh || apt-get install zsh || echo "Whats going wrong!" && exit 1
+ which zsh || yum install zsh || apt-get install zsh || echo "What's going wrong!" && exit 1
  sudo chsh -s $(which zsh)
 #
 # download antigen
  echo "Install Antigen"
+ git clone https://git.io/vbTad ~/.myZsh
  curl -L git.io/antigen > ~/antigen.zsh
 # download custom configure files
- curl -L https://git.io/vbT07 > ~/antigenrc
- curl -L https://git.io/vbTEI > ~/powerline9krc
- curl -L https://git.io/vbTEY > ~/.zshrc
+echo "Copy cfg files"
+  cp ~/.myZsh/* ~
+ # curl -L https://git.io/vbT07 > ~/.antigenrc
+ # curl -L https://git.io/vbTEI > ~/powerline9krc
+ # curl -L https://git.io/vbTEY > ~/.zshrc
 echo ">>>>> Install complite!!! <<<<<<"
