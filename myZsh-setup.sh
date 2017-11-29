0add4d8c3c85050463114ef6da875ea7aa23db7c
Alex Koshelev <xrusts@gmail.com>
2017-11-29 16:44:57 +0500

    fix git clone source

--- myZsh-setup.sh
+++ myZsh-setup.sh
@@ -1,14 +1,14 @@
 #! /bin/bash
 # check and install zsh support CentOS, Fedora an debian like systems.
- which zsh || yum install zsh || apt-get install zsh || echo "What's going wrong!" && exit 1
- sudo chsh -s $(which zsh)
+ # which zsh || yum install zsh || apt-get install zsh || echo "What's going wrong!" && exit 1
+ # sudo chsh -s $(which zsh)
 #
 # download antigen
- echo "Install Antigen"
- git clone https://git.io/vbTad ~/.myZsh
+ echo ">>>>> Install Antigen"
+ git clone https://github.com/XrustS/myZsh.git ~/.myZsh
  curl -L git.io/antigen > ~/antigen.zsh
 # download custom configure files
-echo "Copy cfg files"
+echo ">>>>> Copy cfg files"
   cp ~/.myZsh/* ~
  # curl -L https://git.io/vbT07 > ~/.antigenrc
  # curl -L https://git.io/vbTEI > ~/powerline9krc
